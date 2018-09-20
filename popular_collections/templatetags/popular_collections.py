0a0736feb6520a7f8f2e95aac47927c2fd7ea0d9
ryan28561 <ryan@lightningkite.com>
2018-09-19 18:40:39 -0600

    added template tag for loading popular collections

--- popular_collections/templatetags/popular_collections.py
+++ popular_collections/templatetags/popular_collections.py
@@ -1,9 +1,22 @@
 from django import template
+import importlib
 
 register = template.Library()
 
+PopularCollection = importlib.import_module("saleor-popular-collections.popular_collections.models").PopularCollection
+from saleor.product.models import Collection
 
 @register.inclusion_tag('popular_collections/dashboard/side_nav_inclusion.html',
                         takes_context=True)
 def popular_collections_side_nav(context):
     return context
+
+@register.inclusion_tag('includes/popular_collections.html')
+def popular_collections(super_collection):
+    collections = Collection.objects.filter(popular__isnull=False)
+    if super_collection:
+        for c in collections:
+            if super_collection not in c.super_collections.all():
+                collections = collections.exclude(id=c.id)
+    # collections = collections[0:6]
+    return {"collections": collections}
